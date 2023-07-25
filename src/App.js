062dd5ceacec5bfe19828e5119531791726f2717
1s1kSVIAT <fwsviatoslav@gmail.com>
2023-07-25 19:31:53 +0200

    Header and Tasks blocks

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import Header from "./components/Header";
+import Tasks from "./components/Tasks";
+import { useState } from "react";
+
 
 function App() {
+    const [tasks, setTasks] = useState([
+      {
+        id: 1,
+        text: "Meeting",
+      },
+      {
+        id: 2,
+        text: "play B",
+      },
+    ]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+       <Header/>
+       <Tasks tasks={tasks}/>
     </div>
   );
 }
+
+
 
 export default App;
